2bf613208eba3c330ae176ecdd7451258bfa056c
lyanjnizam <alyajannah13@gmail.com>
2026-07-13 19:48:38 +0800

    streamlit

--- Day 26 - Model Deployment/app.py
+++ Day 26 - Model Deployment/app.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.title('First Streamlit App')
 
 st.write('Hello! Let's build apps!')
+
+name = st.text_input('What is your name?')
+
+age = st.slider('How old are you?', 1, 100, 18)
+
+if name:
+    st.write(f'Nice to meet you {name}!')
+
+    
