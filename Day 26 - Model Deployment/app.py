fdc75f603eda1db0eb51a5faf2b66746d71c41a2
lyanjnizam <alyajannah13@gmail.com>
2026-07-14 10:41:07 +0800

    failed

--- Day 26 - Model Deployment/app.py
+++ Day 26 - Model Deployment/app.py
@@ -4,11 +4,9 @@ st.title('First Streamlit App')
 
 st.write('Hello! Let's build apps!')
 
-name = st.text_input('What is your name?')
+name = st.text_input('What's your name?')
 
-age = st.slider('How old are you?', 1, 100, 18)
+age = st.slider('How old are ya?', 0, 100, 18)
 
 if name:
-    st.write(f'Nice to meet you {name}!')
-
-    
+    st.write(f'Nice to meet ya, {name}!')
